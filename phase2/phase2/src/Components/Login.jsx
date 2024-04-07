92ceec286ec26c562a1fbe3d7ede7db5ea4ab63d
lucifer555-ux <65466352+lucifer555-ux@users.noreply.github.com>
2024-04-07 06:40:03 +0530

    form validation and node file system

--- phase2/phase2/src/Components/Login.jsx
+++ phase2/phase2/src/Components/Login.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Login()
 {
+    const [user, setUser] = useState[""];
+    const [password, setPassword] = useState[""];
+
+    function HandleLogin(e){
+        e.preventDefault();
+    }
     return(
     <div>
     <h1>Login </h1>
+    <form onSubmit={HandleLogin}>
+    <input type = "text" placeholder = "Enter your ID" onChange  = {UserHandler} />
+    <br />  <br /> 
+    <input type = "password" placeholder = "Enter your Password" />
+    <br />  <br /> 
+    <button type = "submit" > Login </button>
 
+    </form>
     </div>
     )
 } 
