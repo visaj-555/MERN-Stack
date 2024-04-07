c9342a2c3cdb096a7e29dd876590822cf3817aee
lucifer555-ux <65466352+lucifer555-ux@users.noreply.github.com>
2024-04-07 14:26:04 +0530

    Done with form validation

--- phase2/phase2/src/Components/Login.jsx
+++ phase2/phase2/src/Components/Login.jsx
@@ -2,19 +2,32 @@ import React, { useState } from "react";
 
 function Login() {
     const [user, setUser] = useState("");
-    const [password, setPassword] = useState("");
+    const [userErr, setUserErr] = useState(false);
+    const [passwordErr, setPassWord] = useState(false);
+
 
     function handleLogin(e) {
         e.preventDefault();
-        // Your login logic here
     }
 
     function userHandler(e) {
+        let ulength = e.target.value.length;
+        if (ulength > 3) {
+            setUserErr(false);
+        } else {
+            setUserErr(true);
+        }
         setUser(e.target.value);
     }
 
     function passwordHandler(e) {
-        setPassword(e.target.value);
+        let plength = e.target.value.length;
+        if (plength > 3) {
+            setPassWord(false);
+        } else {
+            setPassWord(true);
+        }
+        setPassWord(e.target.value);
     }
 
     return (
@@ -22,8 +35,11 @@ function Login() {
             <h1>Login </h1>
             <form onSubmit={handleLogin}>
                 <input type="text" placeholder="Enter your ID" onChange={userHandler} />
+                {userErr ? <span>Not a valid user</span> : ""}
                 <br />  <br /> 
-                <input type="password" placeholder="Enter your Password" onChange={passwordHandler} />
+                <input type="password" placeholder="Enter your Password"  onChange={passwordHandler} />
+                {passwordErr ? <span>Not a valid password</span> : ""}
+
                 <br />  <br /> 
                 <button type="submit"> Login </button>
             </form>
