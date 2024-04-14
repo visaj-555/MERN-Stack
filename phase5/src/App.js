402f9d90505095e91b8fac323df8ff97b224eaf8
lucifer555-ux <65466352+lucifer555-ux@users.noreply.github.com>
2024-04-14 09:21:41 +0530

    Pure component in ReactJs

---
+++ phase5/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import React from 'react'
+import Counter from './components/Counter'
+class  App extends React.Component {
+  constructor()
+  {
+    super();
+    this.state={
+      count:1
+    }
+  }
+ render()
+ {
+  return (
+    <div className="App">
+     <Counter count={this.state.count} />
+      <button 
+      onClick={()=>{this.setState({count:this.state.count+1})}}
+      >Update Count</button>
+    </div>
+  );
+ }
+}
+
+export default App;
